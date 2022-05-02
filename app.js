ca6a01ad12d096fb275b53f8dbc2e4bbc9f34e78
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2022-05-02 13:03:26 -0300

    server running

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/main')
+
+app.listen(3000, ()=>{
+
+ console.log('Servidor funcionando');
+    });
+
+
+app.use('/', indexRouter);
